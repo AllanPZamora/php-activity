11757fc07a690bdad07cdd0f39ed7d2796f664d7
AllanPZamora <gelgelacc1@gmail.com>
2024-10-31 19:51:37 +0800

    .

--- test/act3.php
+++ test/act3.php
@@ -2,12 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">    
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Static Login</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>test</h1>
 
 
     <script src="js/bootstrap.js"></script>
